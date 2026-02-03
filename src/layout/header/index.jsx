334bf0fcf2bfdebfe0fd17a3560f6b434b59c8a6
Fidan <xelilovafidan61@gmail.com>
2026-02-03 11:13:00 +0400

    feat: enhance button styling and update header layout for improved navigation experience

--- src/layout/header/index.jsx
+++ src/layout/header/index.jsx
@@ -1,19 +1,22 @@
 import logo from '../../assets/logo.png'
 import about from '../../assets/about.png'
-import { useState } from 'react';
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
-    const [isHomePage] = useState(window.location.pathname === '/');
+    const [isHomePage] = useState(window.location.pathname === '/')
+    const navigate = useNavigate()
 
   return (
     <>
         {isHomePage ? (
-            <div className="max-w-7xl mx-auto h-100% flex justify-between items-center px-4">
+            <div className="max-w-7xl mx-auto h-100% flex justify-between items-center px-4 relative z-50">
                 <div className='logo'>
                     <img src={logo} alt="logo" className='h-17 w-39.5'/>
                 </div>
-                <div className='bg-linear-to-r from-blue-500 via-purple-500 to-pink-500 p-px rounded-[10px] cursor-pointer'>
-                    <button className="bg-background text-white font-semibold text-[20px] px-5 py-2.5 rounded-lg cursor-pointer flex items-center gap-2 hover:bg-[#ffffff33]">
+                <div className='bg-linear-to-r from-blue-500 via-purple-500 to-pink-500 p-0.5 rounded-[10px] cursor-pointer'>
+                    <button onClick={() => navigate('/about')} 
+                    className="bg-background text-white font-semibold text-[20px] px-5 py-2.5 rounded-lg cursor-pointer flex items-center gap-2 hover:bg-[#596E8F] hover:transition-colors hover:duration-300 hover:ease-in">
                         <img src={about} alt="about"/>
                         Haqqımızda
                     </button>
@@ -26,7 +29,7 @@ const Header = () => {
                         <img src={logo} alt="logo" className='h-17 w-39.5'/>
                     </div>
                     <div className="inline-block p-0.5 rounded-[10px] bg-linear-to-r from-blue-500 via-purple-500 to-pink-500">
-                        <button className="bg-background text-white font-semibold text-[20px] px-5 py-2.5 rounded-lg cursor-pointer hover:bg-[#ffffff33]">
+                        <button className="bg-background text-white font-semibold text-[20px] px-5 py-2.5 rounded-lg cursor-pointer flex items-center gap-2 hover:bg-[#596E8F] hover:transition-colors hover:duration-300 hover:ease-in">
                             Testi Dayandır
                         </button>
                     </div>
