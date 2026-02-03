cbaf765931bc94e0fb1650ed59d40c43ece3c25b
Fidan <xelilovafidan61@gmail.com>
2026-02-03 10:52:35 +0400

    feat: update header layout and enhance about button styling for improved user experience

--- src/layout/header/index.jsx
+++ src/layout/header/index.jsx
@@ -12,8 +12,11 @@ const Header = () => {
                 <div className='logo'>
                     <img src={logo} alt="logo" className='h-17 w-39.5'/>
                 </div>
-                <div className='about'>
-                    <img src={about} alt="about" className='cursor-pointer'/>
+                <div className='bg-linear-to-r from-blue-500 via-purple-500 to-pink-500 p-px rounded-[10px] cursor-pointer'>
+                    <button className="bg-background text-white font-semibold text-[20px] px-5 py-2.5 rounded-lg cursor-pointer flex items-center gap-2 hover:bg-[#ffffff33]">
+                        <img src={about} alt="about"/>
+                        Haqqımızda
+                    </button>
                 </div>
             </div>
         ) : (
@@ -22,8 +25,8 @@ const Header = () => {
                     <div className='logo flex items-center justify-center'>
                         <img src={logo} alt="logo" className='h-17 w-39.5'/>
                     </div>
-                    <div className="inline-block p-[2px] rounded-[10px] bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">
-                        <button className="bg-background text-white font-semibold text-[20px] px-5 py-2.5 rounded-[8px] cursor-pointer">
+                    <div className="inline-block p-0.5 rounded-[10px] bg-linear-to-r from-blue-500 via-purple-500 to-pink-500">
+                        <button className="bg-background text-white font-semibold text-[20px] px-5 py-2.5 rounded-lg cursor-pointer hover:bg-[#ffffff33]">
                             Testi Dayandır
                         </button>
                     </div>
