f44c01e365d8995c655883778e0668144200f35e
Fidan <xelilovafidan61@gmail.com>
2026-02-03 12:13:04 +0400

    feat: update layout of DirectionCard for improved alignment and consistency

--- src/componenets/DirectionCard/index.jsx
+++ src/componenets/DirectionCard/index.jsx
@@ -10,19 +10,21 @@ const DirectionCard = () => {
       {data.map((item) => (
         <div
           key={item.id}
-          className='flex gap-5 px-8 py-6 rounded-xl bg-transparent-[20%] justify-center items-center'
+          className='flex gap-5 px-8 py-6 rounded-xl bg-transparent-[20%] justify-between items-center w-[380px]'
           style={{ backgroundColor: item.color, opacity: '0.8' }}
           onMouseEnter={() => setHovered(item.id)}
           onMouseLeave={() => setHovered(null)}
         >
-          <div className='flex items-center gap-3 flex-wrap'>
-            <img src={item.icon} alt={item.title} className='w-12 h-12' />
-          </div>
-          <div>
-            <h3 className='text-white font-semibold text-lg'>{item.title}</h3>
-            <p className='text-[#A2A8B2] mt-2' style={{ color: item.textColor }}>
-              {hovered === item.id ? 'Ətraflı bax' : item.description}
-            </p>
+          <div className='flex justify-start gap-4'>
+            <div className='flex items-center gap-3 flex-wrap'>
+              <img src={item.icon} alt={item.title} className='w-12 h-12' />
+            </div>
+            <div>
+              <h3 className='text-white font-semibold text-lg text-start'>{item.title}</h3>
+              <p className='text-[#A2A8B2] mt-2 text-start' style={{ color: item.textColor }}>
+                {hovered === item.id ? 'Ətraflı bax' : item.description}
+              </p>
+            </div>
           </div>
           <div className="rounded-[10px] size-6 flex justify-center items-center cursor-pointer" style={{ backgroundColor: item.arrowColor }}>
             <img src={downArrow} alt="downArrow" className="w-6 h-6 object-contain"/>
