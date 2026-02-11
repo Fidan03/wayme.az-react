84258b9dfe942a7afa75ea66c022e0fea8501a06
Fidan <xelilovafidan61@gmail.com>
2026-02-11 05:46:32 +0400

    LoginForm component was created and LoginCardHeader component's bg color was changed

--- src/componenets/LoginCardHeader/index.jsx
+++ src/componenets/LoginCardHeader/index.jsx
@@ -2,15 +2,15 @@ import ProgressBar from '../ProgressBar/index'
 
 const LoginCardHeader = () => {
   return (
-    <div className="w-full rounded-t-[10px] p-5 bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">
+    <div className="w-full rounded-t-[10px] p-5 bg-[#2F4A73]">
 
       <div className="flex items-center justify-between mb-4">
 
-        <p className="text-white font-semibold text-[22px] md:text-[27px] leading-tight">
+        <p className="text-white font-semibold text-[25px] leading-tight">
           İş istiqamətinin müəyyən edilməsi
         </p>
 
-        <p className="text-white font-medium text-[18px] md:text-[22px]">
+        <p className="text-white font-medium text-[18px]">
           Addım 1/5
         </p>
 
